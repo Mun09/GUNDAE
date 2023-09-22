0debc3728b68ca2090f8de39f46e2074fb775a61
Mun Jaeyoung <77109631+Mun09@users.noreply.github.com>
2023-09-22 11:58:55 +0000

    use matter.js

--- playground/src/App.js
+++ playground/src/App.js
@@ -1,5 +1,42 @@
 import './App.css';
 
-const App = () => <div>Hello, world!</div>
+const App = () => {
+  <div>
+    <script src="matter.js"></script>
+    <canvas id="viewport" width="500" height="500"></canvas>
+  </div>
+  
+}
+// module aliases
+var Engine = Matter.Engine,
+    Render = Matter.Render,
+    World = Matter.World,
+    Bodies = Matter.Bodies;
+
+// create an engine
+var engine = Engine.create();
+
+// create a renderer
+var render = Render.create({
+    element: document.body,
+    engine: engine
+});
+
+// create two boxes and a ground
+var boxA = Bodies.rectangle(400, 200, 80, 80);
+var boxB = Bodies.rectangle(450, 50, 80, 80);
+var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+// add all of the bodies to the world
+World.add(engine.world, [boxA, boxB, ground]);
+
+// run the engine
+Engine.run(engine);
+
+// run the renderer
+Render.run(render);
+
+  
+
 
 export default App;
